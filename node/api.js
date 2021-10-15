402d9e1cca4df695ed79b13354271e1ab0bab3af
SmsS <smss.lite@gmail.com>
2021-10-16 01:18:56 +0330

    Fixed docker for nodejs

--- node/api.js
+++ node/api.js
@@ -41,8 +41,14 @@ app.post('/node/sha256', function(req, res) {
 
 dirname = "/home/smss/web/WebProgrammingExercises/frontend"
 app.use(express.static(dirname));
-
+app.get("/node/test", function(req, res) {
+  res.end("YES");
+})
+app.get("/test", function(req, res) {
+  res.end("YES?");
+})
 app.get("/", function(req, res) {
+  res.end("hey")
   res.sendFile(dirname + "/index.html");
   res.end();
 }
