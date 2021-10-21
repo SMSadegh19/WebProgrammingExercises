7a60060c2ff250ace03cc5c6d233143badb8209e
SmsS <smss.lite@gmail.com>
2021-10-21 21:38:58 +0330

    Rename fodlers

---
+++ go/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis-server:6379", // 213.233.179.83
+		Password: "",
+		DB:       0,
+	})
+	_, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+
+	r := gin.Default()
+
+	r.POST("/go/sha256/", func(c *gin.Context) {
+		str := c.Query("string")
+		if len([]rune(str)) < 8 {
+			c.JSON(200, gin.H{
+				"status":     false,
+				"status_str": "String length should be >= 8",
+				"sha256":     nil,
+			})
+		} else {
+			hash_bytes := sha256.Sum256([]byte(str))
+			hash_string := fmt.Sprintf("%x", string(hash_bytes[:]))
+			err := client.Set(hash_string, str, 0).Err()
+			if err != nil {
+				c.JSON(200, gin.H{
+					"status":     false,
+					"status_str": "",
+					"sha256":     hash_string,
+				})
+			} else {
+				c.JSON(200, gin.H{
+					"status":     true,
+					"status_str": err,
+					"sha256":     hash_string,
+				})
+			}
+
+		}
+	})
+	r.GET("/go/sha256/", func(c *gin.Context) {
+		hash := c.Query("sha256")
+		str, err := client.Get(hash).Result()
+		if err != redis.Nil {
+			c.JSON(200, gin.H{
+				"found":  true,
+				"string": str,
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"found":  false,
+				"string": nil,
+			})
+		}
+	})
+	r.Run()
+}
---
+++ nodejs/api.js
@@ -0,0 +1,73 @@
+var express = require('express')
+var app = express()
+var crypto = require('crypto');
+const redis = require("redis");
+
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+});
+
+client.on('connect', function() {
+  console.log('Connected!');
+});
+
+
+let MIN_LENGTH = 8
+
+function check(string){
+  if (string.length < MIN_LENGTH)
+    return [false, "String length should be >= " + MIN_LENGTH]
+  return [true, "OK"];
+}
+
+
+
+app.post('/node/sha256', function(req, res) {
+    var string = req.query.string
+    var check_status = check(string);
+    result = {}
+    result['status'] = check_status[0]
+    result['status_str'] = check_status[1]
+    if (!check_status[0]){
+      result['sha256'] = ''
+    }else{
+      var hash = crypto.createHash('sha256').update(string).digest('hex');
+      result['sha256'] = hash
+      client.set(hash, string);
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
+})
+
+
+app.get('/node/sha256', function(req, res) {
+  var sha256 = req.query.sha256
+  result = {}
+  try {
+        client.get(sha256, async (err, jobs) => {
+            if (err) throw err;
+            res.setHeader('Content-Type', 'application/json');
+            if (jobs) {
+              res.end(JSON.stringify({
+                'found': true,
+                'string': jobs
+              }));     
+            } else {
+              res.end(JSON.stringify({
+                'found': false,
+                'string': ''
+              }));     
+            }
+        });
+    } catch(err) {
+        res.status(500).send({message: err.message});
+    }
+
+  
+})
+
+var server = app.listen(5000, function () {
+  var host = server.address().address
+  var port = server.address().port
+})
